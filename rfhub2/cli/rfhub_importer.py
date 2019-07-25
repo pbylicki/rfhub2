fcaf4c3211dce55d5e7fcd1044c8ad1402fe926c
MaciejWiczk <35737166+MaciejWiczk@users.noreply.github.com>
2019-07-25 06:24:20 +0200

    Adding click args and opts (#23)
    * Adding click args and opts
    * more click options
    * remove packege dependancy on config.py
    * new delete option to flush DB before populating app
    * client.py extracted from population.py

--- rfhub2/cli/rfhub_importer.py
+++ rfhub2/cli/rfhub_importer.py
@@ -1,101 +1,119 @@
-from typing import Tuple, Dict, List
-import robot.libraries
-from robot.libdocpkg import LibraryDocumentation
-from requests import session, post
-from rfhub2.config import APP_INTERFACE, APP_PORT, BASIC_AUTH_USER, BASIC_AUTH_PASSWORD
 import os
 import re
+from robot.libdocpkg import LibraryDocumentation
+import robot.libraries
+from typing import Tuple, Dict, List
+
+from .api_client import Client
+from rfhub2.model import Collection, Keyword
+
 
 RESOURCE_PATTERNS = {".robot", ".txt", ".tsv", ".resource"}
 ALL_PATTERNS = (RESOURCE_PATTERNS | {".xml", ".py"})
 EXCLUDED_LIBRARIES = {"remote", "reserved", "dialogs", "dialogs_jy", "dialogs_py", "dialogs_ipy"}
-PROTOCOL = 'http://'
-API_V1 = 'api/v1'
 
 
-class LibraryPopulation(object):
+class RfhubImporter(object):
 
-    def __init__(self, paths: Tuple[str, ...], no_installed_keywords: bool) -> None:
+    def __init__(self, paths: Tuple[str, ...], no_installed_keywords: bool, client: Client) -> None:
         self.paths = paths
-        self.auth = (BASIC_AUTH_USER, BASIC_AUTH_PASSWORD)
         self.no_installed_keywords = no_installed_keywords
+        self.client = client
+
+    def delete_collections(self) -> None:
+        """
+        Deletes all existing collections.
+        """
+        self.client.check_communication_with_app()
+        collections = self.client.get_collections()
+        collections_id = {collection['id'] for collection in collections}
+        for id in collections_id:
+            self.client.delete_collection(id)
 
     def add_collections(self) -> None:
         """
-        Traverses through paths and adds libraries to rfhub.
-        :return:
+        Adds collections to rfhub.
         """
 
-        def traverse_paths(path: str) -> None:
-            for item in os.listdir(path):
-                full_path = os.path.join(path, item)
-                if os.path.isdir(full_path):
-                    if self._is_library_with_init(full_path):
-                        self.add(full_path)
-                    else:
-                        traverse_paths(full_path)
-                elif os.path.isfile(full_path) and self._is_robot_file(full_path):
-                    self.add(full_path)
-
+        self.client.check_communication_with_app()
         for path in self.paths:
-            traverse_paths(path)
+            self._traverse_paths(path)
         if not self.no_installed_keywords:
             libdir = os.path.dirname(robot.libraries.__file__)
             for item in os.listdir(libdir):
                 if not self._should_ignore(item):
                     self.add(os.path.join(libdir, item))
 
+    def _traverse_paths(self, path: str) -> None:
+        """
+        Traverses through paths and adds libraries to rfhub.
+        Helper function for add_collections.
+        """
+        for item in os.listdir(path):
+            full_path = os.path.join(path, item)
+            if os.path.isdir(full_path):
+                if self._is_library_with_init(full_path):
+                    self.add(full_path)
+                else:
+                    self._traverse_paths(full_path)
+            elif os.path.isfile(full_path) and self._is_robot_keyword_file(full_path):
+                self.add(full_path)
+
     def add(self, path: str) -> None:
         """
         Adds library with keywords to rfhub.
-        :return:
         """
 
-        def _serialise_libdoc() -> Dict:
-            """
-            Serialises libdoc object to dict object.
-            :param libdoc: libdoc input object
-            :param path: library path
-            :return: json object with parameters needed for request post method
-            """
-
-            lib_dict = libdoc.__dict__
-            lib_dict['doc_format'] = lib_dict.pop('_setter__doc_format')
-            for key in ('_setter__keywords', 'inits', 'named_args'):
-                lib_dict.pop(key)
-            lib_dict['path'] = path
-            return lib_dict
-
-        def _serialise_keywords() -> List[Dict[str, str]]:
-            keywords = [keyword.__dict__ for keyword in libdoc.keywords]
-            for keyword in keywords:
-                keyword.pop('tags')
-                if keyword["args"]:
-                    keyword["args"] = str([str(item) for item in keyword["args"]]).replace("\'", "\"")
-                else:
-                    keyword["args"] = ""
-            return keywords
-
         libdoc = LibraryDocumentation(path)
-        serialised_keywords = _serialise_keywords()
-        serialised_libdoc = _serialise_libdoc()
-        s = session()
-        coll_req = self._post_request(s, 'collections', serialised_libdoc)
-        if coll_req.status_code == 201:
+        serialised_keywords = self._serialise_keywords(libdoc)
+        serialised_libdoc = self._serialise_libdoc(libdoc, path)
+        coll_req = self.client.add_collection(serialised_libdoc)
+        if coll_req['name'] == serialised_libdoc['name']:
+            collection_id = coll_req['id']
             for keyword in serialised_keywords:
-                collection_id = coll_req.json()["id"]
-                keyword["collection_id"] = collection_id
-                kwd_req = self._post_request(s, 'keywords', keyword)
+                keyword['collection_id'] = collection_id
+                self.client.add_keyword(keyword)
             print(f'{libdoc.name} library with {len(serialised_keywords)} keywords loaded.')
         else:
             print(f'{libdoc.name} library was not loaded!')
+
+    def _serialise_libdoc(self, libdoc, path: str) -> Collection:
+        """
+        Serialises libdoc object to Collection object.
+        :param libdoc: libdoc input object
+        :param path: library path
+        :return: Collection object
+        """
+
+        lib_dict = libdoc.__dict__
+        lib_dict['doc_format'] = lib_dict.pop('_setter__doc_format')
+        for key in ('_setter__keywords', 'inits', 'named_args'):
+            lib_dict.pop(key)
+        lib_dict['path'] = path
+        return lib_dict
+
+    def _serialise_keywords(self, libdoc) -> Keyword:
+        """
+        Serialises keywords to Keyword object.
+        :param :libdoc input object
+        :return: Keyword object
+        """
+
+        keywords = [keyword.__dict__ for keyword in libdoc.keywords]
+        for keyword in keywords:
+            keyword.pop('tags')
+            if keyword["args"]:
+                keyword["args"] = str([str(item) for item in keyword["args"]]).replace("\'", "\"")
+            else:
+                keyword["args"] = ""
+        return keywords
 
     @staticmethod
     def _is_library_with_init(path: str) -> bool:
         return os.path.isfile(os.path.join(path, '__init__.py')) and \
             len(LibraryDocumentation(path).keywords) > 0
 
-    def _is_robot_file(self, file: str) -> bool:
+    def _is_robot_keyword_file(self, file: str) -> bool:
         return self._is_library_file(file) or \
                self._is_libdoc_file(file) or \
                self._is_resource_file(file)
@@ -158,12 +176,3 @@ class LibraryPopulation(object):
                             re.match(r'(User )?Keywords?', match.group(1), re.IGNORECASE)):
                         found_keyword_table = True
         return found_keyword_table
-
-    def _post_request(self, session: session, endpoint: str, data: Dict) -> post:
-        """
-        Posts request to collections or keywords endpoint
-        """
-        request = session.post(url=f'{PROTOCOL}{APP_INTERFACE}:{APP_PORT}/{API_V1}/{endpoint}/',
-                           auth=self.auth, json=data,
-                           headers={"Content-Type": "application/json", "accept": "application/json"})
-        return request

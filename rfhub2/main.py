42ca650149adcc2552ff79e4bcf81edd763c3415
Pawel Bylicki <pawelkbylicki@gmail.com>
2019-08-30 23:53:56 +0200

    Remove recreating sample data on startup

--- rfhub2/main.py
+++ rfhub2/main.py
@@ -1,8 +1,6 @@
 from rfhub2.app import create_app
 from rfhub2.db.init_db import init_db
-from rfhub2.db.sample_data import recreate_data
 from rfhub2.db.session import db_session
 
 init_db(db_session)
-recreate_data(db_session)
 app = create_app()

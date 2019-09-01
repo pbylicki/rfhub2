4fd66e7a465df6fd4a0c9b16789f44f1a927f13b
Paweł Bylicki <pawelkbylicki@gmail.com>
2019-09-01 14:07:59 +0200

    Fix application routing to enable accessing frontend routes (#37)

---
+++ tests/ui/ui_router_tests.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import unittest
+from starlette.testclient import TestClient
+
+from rfhub2.app import create_app
+
+
+class UIRouterTest(unittest.TestCase):
+
+    INDEX_FILE = (
+        Path(__file__).parent / ".." / ".." / "rfhub2" / "templates" / "index.html"
+    )
+
+    def setUp(self) -> None:
+        self.app = create_app()
+        self.client: TestClient = TestClient(self.app)
+
+    def test_ui_routes_should_get_index_html_file(self):
+        with open(self.INDEX_FILE) as f:
+            expected_body = f.read()
+        routes = ("/", "/search/?q=a", "/keywords/123/", "/keywords/123/456/")
+        for route in routes:
+            with self.subTest(route=route):
+                response = self.client.get(route)
+                self.assertEqual(response.status_code, 200)
+                self.assertEqual(response.text, expected_body)

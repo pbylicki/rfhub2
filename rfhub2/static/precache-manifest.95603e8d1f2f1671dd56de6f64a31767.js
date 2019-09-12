aecccc225313aea885afb51864e290d1027f1d99
Paweł Bylicki <pawelkbylicki@gmail.com>
2019-09-12 20:24:42 +0200

    Fix another race condition with CollectionStore search term (#52)

--- rfhub2/static/precache-manifest.95603e8d1f2f1671dd56de6f64a31767.js
+++ rfhub2/static/precache-manifest.95603e8d1f2f1671dd56de6f64a31767.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e5656812e9be5837b63b83e67d8e6028",
+    "revision": "6cec1bb3e9f1725d8bd2530c3045856a",
     "url": "/static/index.html"
   },
   {
-    "revision": "4298a5831defbaad2b94",
+    "revision": "7e1aa1f8ff8f6f532ed8",
     "url": "/static/static/css/main.2733a44a.chunk.css"
   },
   {
-    "revision": "5199887dfd350eaf36d0",
-    "url": "/static/static/js/2.df8caaff.chunk.js"
+    "revision": "9293c7d9b53537d4170e",
+    "url": "/static/static/js/2.5121adea.chunk.js"
   },
   {
-    "revision": "4298a5831defbaad2b94",
-    "url": "/static/static/js/main.17ad586e.chunk.js"
+    "revision": "7e1aa1f8ff8f6f532ed8",
+    "url": "/static/static/js/main.6e17c374.chunk.js"
   },
   {
     "revision": "517784e95ca40345faae",

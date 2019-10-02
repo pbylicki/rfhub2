a96d881c8a610121d87b61ebff50a5afde3c34f3
Pawel Bylicki <pawelkbylicki@gmail.com>
2019-10-02 23:59:43 +0200

    Change python executable

--- scripts/install_package_in_venv.sh
+++ scripts/install_package_in_venv.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-python3 -m venv atests-venv
+# using workaround for https://github.com/travis-ci/travis-ci/issues/8589
+/opt/python/3.6/bin/python -m venv atests-venv
 source atests-venv/bin/activate
 pip3 install $(find dist -type f -iname "rfhub2-*.whl")

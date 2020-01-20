4b0475048c27e9d2d5a40404d62af01a85d75423
MarcinMaciaszek <50177134+MarcinMaciaszek@users.noreply.github.com>
2020-01-20 22:28:44 +0100

    Added elipsis to long keyword names and tooltip to keywords(#133) (#144)
    * Added elipsis to long keyword names and tooltip to keywords(#133)
    
    * Changing locator
    
    * Fixes based on comments(#133).
    
    * Fix for failing test
    
    Co-authored-by: Maciej Wiczk <35737166+MaciejWiczk@users.noreply.github.com>

--- frontend/src/components/DrawerCollectionList.tsx
+++ frontend/src/components/DrawerCollectionList.tsx
@@ -12,6 +12,8 @@ import { StoreProps } from '../types/PropsTypes'
 import { List } from '@material-ui/core';
 import { Collection } from '../types/ModelTypes';
 import { CollectionStore } from '../stores/CollectionStore';
+import Tooltip from 'react-tooltip-lite'
+import EllipsisText from "react-ellipsis-text";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -53,15 +55,34 @@ const DrawerCollectionListItem: React.FC<DrawerCollectionListItemProps> = observ
       <Collapse in={isSelected(collection.id)} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
           <ListItem button className={classes.nested}>
-            <Link to={`/keywords/${collection.id}`}><ListItemText primary="Overview" /></Link>
+            <Link to={`/keywords/${collection.id}`}><ListItemText primary="Overview" disableTypography /></Link>
           </ListItem>
-          {collection.keywords.map(keyword => (
-            <ListItem button key={keyword.id} className={classes.nested}>
-              <Link to={`/keywords/${collection.id}/${keyword.id}/`}>
-                <ListItemText primary={keyword.name} />
-              </Link>
-            </ListItem>
-          ))}
+          {collection.keywords.map(keyword => {
+            if (keyword.name.length > 35) {
+              return (
+                <Tooltip content={keyword.name} direction="right" distance={26}>
+                  <ListItem button key={keyword.id} className={classes.nested}>
+                    <Link to={`/keywords/${collection.id}/${keyword.id}/`}>
+                      <ListItemText disableTypography>
+                        <EllipsisText
+                          text={keyword.name}
+                          length='35'
+                        />
+                      </ListItemText>
+                    </Link>
+                  </ListItem>
+                </Tooltip>
+              )
+            } else {
+              return (
+                <ListItem button key={keyword.id} className={classes.nested}>
+                  <Link to={`/keywords/${collection.id}/${keyword.id}/`}>
+                    <ListItemText primary={keyword.name} disableTypography />
+                  </Link>
+                </ListItem>
+              )
+            }
+          })}
         </List>
       </Collapse>
     </React.Fragment>

b013c70a21a47dde6fa2dac43339685d09499000
Pawel Bylicki <pawelkbylicki@gmail.com>
2020-01-25 12:12:54 +0100

    Remove search character limit

--- rfhub2/static/precache-manifest.aae436e5f05bf42f990713f3b9965c0c.js
+++ rfhub2/static/precache-manifest.aae436e5f05bf42f990713f3b9965c0c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f8c0cd9dde470b60591db32d89c835b6",
+    "revision": "f5eff942bde21e0e167fcbc2adf65171",
     "url": "/static/index.html"
   },
   {
-    "revision": "07adfbd8d2387ae8d04b",
+    "revision": "6abc9ff12c0768f1f6ea",
     "url": "/static/static/css/main.fc37acba.chunk.css"
   },
   {
-    "revision": "ca4ac08942e73d5c61c3",
-    "url": "/static/static/js/2.0cb0162c.chunk.js"
+    "revision": "9998b1a094739e712ee1",
+    "url": "/static/static/js/2.48375331.chunk.js"
   },
   {
-    "revision": "07adfbd8d2387ae8d04b",
-    "url": "/static/static/js/main.9d0402f6.chunk.js"
+    "revision": "6abc9ff12c0768f1f6ea",
+    "url": "/static/static/js/main.407c4116.chunk.js"
   },
   {
     "revision": "517784e95ca40345faae",

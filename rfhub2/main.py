3c05da2aed5690e0908da0d86cd063fe027ca064
Paweł Bylicki <pawelkbylicki@gmail.com>
2020-04-03 00:26:06 +0200

    Add initial alembic migration (#209)
    * Add initial alembic migration
    
    * Filter out alembic files from coverage
    
    * Bump version

--- rfhub2/main.py
+++ rfhub2/main.py
@@ -1,6 +1,6 @@
 from rfhub2.app import create_app
-from rfhub2.db.init_db import init_db
-from rfhub2.db.session import db_session
+from rfhub2.db.migrate import migrate_db
+from rfhub2.db.session import engine
 
-init_db(db_session)
+migrate_db(engine)
 app = create_app()

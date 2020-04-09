fcfcc4bfabad662bbd8b8af0bc76fc0bce8286a6
MaciejWiczk <mezazel@gmail.com>
2020-04-09 17:59:48 +0200

    Fix for missing alembic.ini from package, and path to scripts

--- rfhub2/db/migrate.py
+++ rfhub2/db/migrate.py
@@ -4,7 +4,7 @@ from alembic import command
 from sqlalchemy.engine import Connection, Engine
 from sqlalchemy.engine.reflection import Inspector
 from pathlib import Path
-from os import chdir
+from os import chdir, getcwd
 
 
 def has_tables(connection: Connection) -> bool:
@@ -23,9 +23,12 @@ def migrate_db(engine: Engine) -> None:
     alembic_cfg_path = Path(__file__).resolve().parent.parent.parent / "rfhub2" / "alembic.ini"
     alembic_cfg = Config(alembic_cfg_path)
     with engine.begin() as connection:
+        cwd = getcwd()
         chdir(Path(__file__).resolve().parent.parent / "alembic" / "versions")
         # check if database has not been migrated yet with alembic
         if has_tables(connection) and not has_revision(connection):
             # stamp database with the initial revision id, representing pre-alembic database schema
             command.stamp(alembic_cfg, "c54a916ec6c8")
         command.upgrade(alembic_cfg, "head")
+        chdir(cwd)
+
